cf09b06f8392a41108a60b7b4835448c31d5e681
iceyao <yao3690093@gmail.com>
2024-10-13 02:05:42 +0800

    update pycryptodome in requirements.txt

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ from volcengine import VERSION
 
 install_requires = [
     "requests>=2.25.1",
-    "retry==0.9.2",
-    "pytz==2020.5",
-    "pycryptodome==3.9.9",
+    "retry>=0.9.2",
+    "pytz>=2020.5",
+    "pycryptodome>=3.9.9",
     "protobuf>=3.18.3",
     "google>=3.0.0",
     "six>=1.0",
 ]
 
 setup(
-    name="volcengine",
+    name="volcengine-compat",
     version=VERSION,
     keywords=("pip", "volcengine", "volc-sdk-python"),
-    description="The Volcengine SDK for Python",
+    description="Be Compatible with the Volcengine SDK for Python, The version of package dependencies has been modified. like pycryptodome, pytz.",
     license="MIT License",
 
-    url="https://github.com/Volcengine/volc-sdk-python",
-    author="Volcengine SDK",
+    url="https://github.com/yaoice/volc-sdk-python",
+    author="Ice Yao",
 
     packages=find_packages(),
     include_package_data=True,
